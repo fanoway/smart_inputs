4b6e70163b8f8286f07176c6d088ff8dc8c0ee08
fanoway <86997883+fanoway@users.noreply.github.com>
2021-12-27 07:06:19 -0800

    REV: Roll to 0.4.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, fanoway"
 author = "fanoway"
 
 # The full version, including alpha/beta/rc tags
-release = "0.3.1"
+release = "0.4.0"
 
 
 # -- General configuration ---------------------------------------------------
